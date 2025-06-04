446114fa8410b8b056eccd26256fec6c4f5d14af
Aditya1603200 <adityadhage1603@gmail.com>
2025-06-04 09:50:50 +0530

    Map Filter Reduce

---
+++ JS/03-06-2025/script.js
@@ -0,0 +1,133 @@
+// Sorts alphabetically, in dictionary format
+let numbers = [43534, 6, 45634, 33, 534, 32332, 3254, 4356, 56]
+// numbers.reverse() // only reverses the array elements if not used after sort
+// console.log(numbers)
+// console.log(numbers.sort())
+// console.log(numbers.reverse())
+
+// let names = ["aditya", "aaditya", "anushka", "atharva", "chetana", "dwij"]
+// console.log(names.reverse())
+// console.log(names.sort())
+// console.log(names.reverse())
+
+// let newArr = names.concat(numbers)
+// console.log(newArr)
+// console.log(newArr.reverse())
+// //Numbers get priority over characters in sorting
+// console.log(newArr.sort())
+// console.log(newArr.reverse())
+
+// let cartoons = ["doaeramon", "shinchan", "hagemaru", "bheem"]
+// let singleCartoon = cartoons.join("❤️✨")
+// console.log(singleCartoon)
+
+// let msg = "Morning Class"
+// let newMsg = msg.split('')
+// console.log(newMsg)
+
+// It is a higher order function that accepts callback function as an argument
+
+// let evenNumbers = numbers.filter(val => {
+//   return val % 2 == 0
+// })
+// console.log(evenNumbers)
+
+// let names = ["pooja", "priti", "payal", "nishidha", "sakshi"]
+// let pNames = names.filter(ele => {
+//   return ele.includes("p")
+// })
+// console.log(pNames)
+
+// 10 filter program 10 condition 10 array and all conditions to be different
+
+//Numbers greater than 30
+// let Numbers = [10, 25, 35, 60, 5, 80];
+// let bigNums = Numbers.filter((ele) => {
+//     return ele > 30;
+// });
+// console.log(bigNums); 
+
+// //Words with length > 4
+// let Words = ["cat", "house", "apple", "pen", "elephant"];
+// let lenghtGreater = Words.filter((ele) => {
+//     return ele.length > 4;
+// });
+// console.log(lenghtGreater); 
+
+// //Cities starting with M
+// let Cities = ["Mumbai", "Pune", "Delhi", "Madurai", "Mysore"];
+// let mCities = Cities.filter((ele) => {
+//     return ele.startsWith("M");
+// });
+// console.log(mCities); // ["Mumbai", "Madurai", "Mysore"]
+
+// //Even numbers
+// let Values = [1, 2, 3, 4, 5, 6, 7, 8];
+// let evenVal = Values.filter((ele) => {
+//     return ele % 2 === 0;
+// });
+// console.log(evenVal); 
+
+// //Passing marks greater than or eqaul to 35
+// let Scores = [20, 35, 40, 28, 90, 50];
+// let passed = Scores.filter((ele) => {
+//     return ele >= 35;
+// });
+// console.log(passed); 
+
+// //Fruits that contain a
+// let Fruits = ["apple", "banana", "grape", "kiwi", "orange"];
+// let aFruits = Fruits.filter((ele) => {
+//     return ele.includes("a");
+// });
+// console.log(aFruits); 
+
+// //age > 18
+// let Ages = [12, 19, 25, 17, 40, 10];
+// let adults = Ages.filter((ele) => {
+//     return ele > 18;
+// });
+// console.log(adults); 
+
+// //numbers less than 10
+// let nums = [5, 12, 3, 20, 8];
+// let smallNums = nums.filter((ele) => {
+//     return ele < 10;
+// });
+// console.log(smallNums); 
+
+// // names ending with a
+// let names = ["Seema", "Ravi", "Neha", "Karan", "Meena"];
+// let aEnding = names.filter((ele) => {
+//     return ele.endsWith("a");
+// });
+// console.log(aEnding); 
+
+// //odd numbers
+// numbers = [1, 2, 3, 4, 5, 6];
+// let oddNumbers = numbers.filter((ele) => {
+//     return ele % 2 !== 0;
+// });
+// console.log(oddNumbers); 
+
+// //names that contain "i"
+// let students = ["Nina", "Amit", "Riya", "Rohan"];
+// let Namesi = students.filter((ele) => {
+//     return ele.includes("i");
+// });
+// console.log(Namesi); 
+
+// //prices less than 100
+// let prices = [120, 50, 200, 80, 99];
+// let Items = prices.filter((ele) => {
+//     return ele < 100;
+// });
+// console.log(Items); 
+
+// //strings longer than 3 letters
+// let animals = ["cat", "dog", "lion", "tiger"];
+// let longNames = animals.filter((ele) => {
+//     return ele.length > 3;
+// });
+// console.log(longNames);
+
---
+++ JS/04-06-2025/script.js
@@ -0,0 +1,23 @@
+// It is an array looping technique used to map values to each element
+// It returns new array result for array elements
+// Also a higher order function
+
+let currentAge = [10, 20, 30, 40]
+let ageAfterFive = currentAge.map(ele => {
+  return ele + 5
+})
+console.log(ageAfterFive)
+
+let names = ["aditya", "anushka", "chetana", "atharva"]
+
+let Anames = names.map(ele => {
+  return ele.startsWith("a")
+})
+
+console.log(Anames)
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let sum = numbers.reduce((e1, e2) => {
+  return e1 + e2
+})
+console.log(sum)
